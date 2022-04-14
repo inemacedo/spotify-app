11a4e5443f73e8c3e09943a43ba9ab309231132e
inemacedo <93352798+inemacedo@users.noreply.github.com>
2022-04-13 21:50:21 -0300

    create-react-app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header";
+import AlbumsList from "./components/AbumsList";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <AlbumsList />
+    </div>
+  );
+}
+
+export default App;
