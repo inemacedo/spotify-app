56ced48b2311f0658175d23af54c4a0941fb5236
inemacedo <93352798+inemacedo@users.noreply.github.com>
2022-04-18 13:01:01 -0300

    disable button & input when searching

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "../styles/Header.css";
 import FilterByArtist from "./FilterByArtist";
 
 function Header() {
+  const albums = useSelector((state) => state);
+  const withAlbums = albums.length > 0 ? "with-albums" : "";
+
   return (
-    <div className="header-background d-flex flex-column align-items-center justify-content-center">
+    <div
+      className={`header-background ${withAlbums} d-flex flex-column align-items-center justify-content-center`}
+    >
       <div className="container">
         <h1 className="text-white mb-0 fw-bold">Houlakify</h1>
         <h2 className="text-white fs-4 mb-3">Your favourite albums</h2>
